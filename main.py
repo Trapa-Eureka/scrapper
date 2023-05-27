97358c7e396541162d257e9167a79df4c3fa67cd
Trapa-Eureka <son199@hanmail.net>
2023-05-28 00:04:43 +0900

    Desired search results: text information of title and subtitle.

--- main.py
+++ main.py
@@ -2,27 +2,43 @@ from requests import get
 from bs4 import BeautifulSoup
 
 base_url = "https://tribune.net.ph/?s="
-search_term = "business"
+search_term = "immigration"
 
 response = get(f"{base_url}{search_term}")
 
-# print(response)
-
 if response.status_code != 200:
-    print("can't request website")
+    print("Can't request website")
 else:
-    # print(response)
+    results = []
     soup = BeautifulSoup(response.text, "html.parser")
     news = soup.find_all('div', class_="border-dark")
     for news_section in news:
-        news_post = news_section.find_all('div', class_="col-lg-6")
-        for post in news_post:
+        news_divs = news_section.find_all('div', recursive=False)
+        if len(news_divs) > 1:
+            post = news_divs[1]
             anchors = post.find_all('a')
-            anchor = anchors[1]
-            link = anchor['href']
-            title = anchor.find("h3")
-            subtitle = anchor.find("div")
-            # title, subtitle = anchor.find("h3", "div")
-            print(title, subtitle)
-            print("//////////////////////")
-            print("//////////////////////")
+            for anchor in anchors:
+                link = anchor['href']
+                title_element = anchor.find("h3")
+                subtitle_element = anchor.find("div")
+
+                if title_element is not None and subtitle_element is not None:
+                    if title_element is not None:
+                        title = title_element.get_text().strip()
+                    else:
+                        title = ""
+
+                    if subtitle_element is not None:
+                        subtitle = subtitle_element.get_text().strip()
+                    else:
+                        subtitle = ""
+
+                    news_data = {
+                        'title': title,
+                        'subtitle': subtitle
+                    }
+                    results.append(news_data)
+
+    for result in results:
+        print(result)
+        print("////////////////")
